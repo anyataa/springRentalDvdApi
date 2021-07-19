7e23dd480dc901b17a9dc3a07d30efa97bb5d1ab
Anya Tamara <anyatmr@gmail.com>
2021-07-19 15:12:04 +0700

    initialize wilayah app

---
+++ wilayah_project/src/main/java/com/wilayah/wilayah_project/WilayahProjectApplication.java
@@ -0,0 +1,13 @@
+package com.wilayah.wilayah_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WilayahProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WilayahProjectApplication.class, args);
+	}
+
+}
---
+++ wilayah_project/src/test/java/com/wilayah/wilayah_project/WilayahProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wilayah.wilayah_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WilayahProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
